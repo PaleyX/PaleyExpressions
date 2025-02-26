b705fd494a5da36d0bc6cfe1fc326f7e451b5107
PaleyX <jonathan.palethorpe@hotmail.co.uk>
2025-02-26 12:25:17 +0000

    Getting started

---
+++ PaleyExpressions/Token.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaleyExpressions
+{
+    internal class Token
+    {
+        private readonly TokenType _type;
+        private readonly string _lexeme;
+        private readonly object _literal;
+        private readonly int _line;
+
+        Token(TokenType type, String lexeme, Object literal, int line)
+        {
+            _type = type;
+            _lexeme = lexeme;
+            _literal = literal;
+            _line = line;
+        }
+
+        public override string ToString()
+        {
+            return $"{_type} {_lexeme} {_literal}";
+        }
+    }
+}
