ece13d12c34b7d66882023bb4ff061d0c786ef55
PaleyX <jonathan.palethorpe@hotmail.co.uk>
2025-02-28 11:53:35 +0000

    Added visitor

---
+++ PaleyExpressions/Expr.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PaleyExpressions.Expr;
+
+namespace PaleyExpressions
+{
+    internal abstract class Expr
+    {
+        internal interface IVisitor<R>
+        {
+            //R VisitAssignExpr(Assign expr);
+            R VisitBinaryExpr(Binary expr);
+            //R VisitCallExpr(Call expr);
+            //R VisitGetExpr(Get expr);
+            R VisitGroupingExpr(Grouping expr);
+            R VisitLiteralExpr(Literal expr);
+            R VisitLogicalExpr(Logical expr);
+            //R VisitSetExpr(Set expr);
+            //R VisitSuperExpr(Super expr);
+            //R VisitThisExpr(This expr);
+            R VisitUnaryExpr(Unary expr);
+            //R VisitVariableExpr(Variable expr);
+        }
+
+        // Nested Expr classes here...
+
+        internal abstract R Accept<R>(IVisitor<R> visitor);
+
+        internal class Binary(Expr left, Token op, Expr right) : Expr
+        {
+            internal Expr Left { get; } = left;
+            internal Token Operator { get; } = op;
+            internal Expr Right { get; } = right;
+
+            internal override R Accept<R>(IVisitor<R> visitor) => visitor.VisitBinaryExpr(this);
+        }
+
+        internal class Grouping(Expr expr) : Expr
+        {
+            internal Expr Expression { get; } = expr;
+
+            internal override R Accept<R>(IVisitor<R> visitor) => visitor.VisitGroupingExpr(this);
+        }
+
+        internal class Literal(object? value) : Expr
+        {
+            internal object? Value { get; } = value;
+
+            internal override R Accept<R>(IVisitor<R> visitor) => visitor.VisitLiteralExpr(this);
+        }
+
+        internal class Logical(Expr left, Token op, Expr right) : Expr
+        {
+            internal Expr Left { get; } = left;
+            internal Token Operator { get; } = op;
+            internal Expr Right { get; } = right;
+
+            internal override R Accept<R>(IVisitor<R> visitor) => visitor.VisitLogicalExpr(this);
+        }
+
+        internal class Unary(Token op, Expr right) : Expr
+        {
+            internal Token Operator { get; } = op;
+            internal Expr Right { get; } = right;
+
+            internal override R Accept<R>(IVisitor<R> visitor) => visitor.VisitUnaryExpr(this);
+        }
+    }
+}
---
+++ UnitTests/ScannerTests.cs
@@ -0,0 +1,39 @@
+﻿using static PaleyExpressions.TokenType;
+
+namespace UnitTests
+{
+    public class ScannerTests
+    {
+        [Fact]
+        public void Scanner_Tokenises_Correctly()
+        {
+            var scanner = new PaleyExpressions.Scanner("1+true+gort");
+
+            var tokens = scanner.ScanTokens();
+
+            Assert.Equal(5 + 1, tokens.Count);
+
+            Assert.Equal(NUMBER, tokens[0].TokenType);
+            Assert.Equal("1", tokens[0].Lexeme);
+            Assert.Equal(1d, tokens[0].Literal);
+
+            Assert.Equal(PLUS, tokens[1].TokenType);
+            Assert.Equal("+", tokens[1].Lexeme);
+            Assert.Null(tokens[1].Literal);
+
+            Assert.Equal(TRUE, tokens[2].TokenType);
+            Assert.Equal("true", tokens[2].Lexeme);
+            Assert.Null(tokens[2].Literal);
+
+            Assert.Equal(PLUS, tokens[3].TokenType);
+            Assert.Equal("+", tokens[3].Lexeme);
+            Assert.Null(tokens[3].Literal);
+
+            Assert.Equal(IDENTIFIER, tokens[4].TokenType);
+            Assert.Equal("gort", tokens[4].Lexeme);
+            Assert.Null(tokens[4].Literal);
+
+            Assert.Equal(EOF, tokens[5].TokenType);
+        }
+    }
+}
