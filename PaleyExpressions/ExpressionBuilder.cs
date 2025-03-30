9c538b2bc17ce176534ed7b2d9d4b451b3302afe
PaleyX <jonathan.palethorpe@hotmail.co.uk>
2025-03-30 10:30:35 +0100

    Conversions for the ExpressionBuilder

--- PaleyExpressions/ExpressionBuilder.cs
+++ PaleyExpressions/ExpressionBuilder.cs
@@ -1,6 +1,5 @@
 ﻿//#define USEPROOF
 
-using System.Diagnostics;
 using System.Linq.Expressions;
 using System.Reflection;
 using static PaleyExpressions.TokenType;
@@ -25,56 +24,56 @@ internal class ExpressionBuilder : Expr.IVisitor<Expression>
         {
             case GREATER:
             {
-                var convert = Conversions<double>(lhs, rhs);
-                return Expression.GreaterThan(convert.lhc, convert.rhc);
+                var (lhc, rhc) = Conversions<double>(lhs, rhs);
+                return Expression.GreaterThan(lhc, rhc);
             }
             case GREATER_EQUAL:
             {
-                var convert = Conversions<double>(lhs, rhs);
-                return Expression.GreaterThanOrEqual(convert.lhc, convert.rhc);
+                var (lhc, rhc) = Conversions<double>(lhs, rhs);
+                return Expression.GreaterThanOrEqual(lhc, rhc);
             }
             case LESS:
             {
-                var convert = Conversions<double>(lhs, rhs);
-                return Expression.LessThan(convert.lhc, convert.rhc);
+                var (lhc, rhc) = Conversions<double>(lhs, rhs);
+                return Expression.LessThan(lhc, rhc);
             }
             case LESS_EQUAL:
             {
-                var convert = Conversions<double>(lhs, rhs);
-                return Expression.LessThanOrEqual(convert.lhc, convert.rhc);
+                var (lhc, rhc) = Conversions<double>(lhs, rhs);
+                return Expression.LessThanOrEqual(lhc, rhc);
             }
             case MINUS:
             {
-                var convert = Conversions<double>(lhs, rhs);
-                return Expression.Subtract(convert.lhc, convert.rhc);
+                var (lhc, rhc) = Conversions<double>(lhs, rhs);
+                return Expression.Subtract(lhc, rhc);
             }
             case BANG_EQUAL:
             {
                 var equals = typeof(object).GetMethod("Equals", [typeof(object), typeof(object)]);
-                var convert = Conversions<object>(lhs, rhs);
-                return Expression.Not(Expression.Call(equals!, [convert.lhc, convert.rhc]));
+                var (lhc, rhc) = Conversions<object>(lhs, rhs);
+                return Expression.Not(Expression.Call(equals!, [lhc, rhc]));
             }
             case EQUAL_EQUAL:
             {
                 var equals = typeof(object).GetMethod("Equals", [typeof(object), typeof(object)]);
-                var convert = Conversions<object>(lhs, rhs);
-                return Expression.Call(equals!, [convert.lhc, convert.rhc]);
+                var (lhc, rhc) = Conversions<object>(lhs, rhs);
+                return Expression.Call(equals!, [lhc, rhc]);
             }
             case PLUS:
             {
                 var plus = typeof(Helpers).GetMethod("Plus", BindingFlags.NonPublic | BindingFlags.Static);
-                var convert = Conversions<object>(lhs, rhs);
-                return Expression.Call(plus!, [convert.lhc, convert.rhc]);
+                var (lhc, rhc) = Conversions<object>(lhs, rhs);
+                return Expression.Call(plus!, [lhc, rhc]);
             }
             case SLASH:
             {
-                var convert = Conversions<double>(lhs, rhs);
-                return Expression.Divide(convert.lhc, convert.rhc);
+                var (lhc, rhc) = Conversions<double>(lhs, rhs);
+                return Expression.Divide(lhc, rhc);
             }
             case STAR:
             {
-                var convert = Conversions<double>(lhs, rhs);
-                return Expression.Multiply(convert.lhc, convert.rhc);
+                var (lhc, rhc) = Conversions<double>(lhs, rhs);
+                return Expression.Multiply(lhc, rhc);
             }
         }
 
@@ -90,7 +89,9 @@ internal class ExpressionBuilder : Expr.IVisitor<Expression>
         {
             var built = Build(item.value);
 
-            if (parameters[item.index].ParameterType == typeof(Func<object?>))
+            var parameterType = parameters[item.index].ParameterType;
+
+            if (parameterType == typeof(Func<object?>))
             {
 #if USEPROOF
                 var writeLineMethod = typeof(Console).GetMethod("WriteLine", [typeof(string)]);
@@ -106,30 +107,47 @@ internal class ExpressionBuilder : Expr.IVisitor<Expression>
 #else
                 var converted = Expression.Convert(built, typeof(object));
 #endif
-                var lambda = Expression.Lambda<Func<object>>(converted/*, GetParameters()*/);
+                var lambda = Expression.Lambda<Func<object>>(converted);
                 args.Add(lambda);
+                continue;
             }
-            else
+
+            var x = new[] { typeof(double), typeof(string), typeof(bool) };
+
+            if (x.Contains(parameterType))
             {
-                args.Add(built);
+                var cast = Expression.Convert(built, parameterType);
+                args.Add(cast);
+                continue;
             }
+
+            //else if (parameters[item.index].ParameterType == typeof(double))
+            //{
+            //    var cast = Expression.Convert(built, typeof(double));
+            //    args.Add(cast);
+            //}
+            //else if (parameters[item.index].ParameterType == typeof(string))
+            //{
+            //    var cast = Expression.Convert(built, typeof(string));
+            //    args.Add(cast);
+            //}
+            //else if (parameters[item.index].ParameterType == typeof(bool))
+            //{
+            //    var cast = Expression.Convert(built, typeof(bool));
+            //    args.Add(cast);
+            //}
+            //else
+            //{
+                args.Add(built);
+            //}
         }
 
         return Expression.Call(expr.Function, [.. args]);
-
-
-        throw new NotImplementedException();
     }
 
-    public Expression VisitGroupingExpr(Expr.Grouping expr)
-    {
-        return Build(expr.Expression);
-    }
+    public Expression VisitGroupingExpr(Expr.Grouping expr) => Build(expr.Expression);
 
-    public Expression VisitLiteralExpr(Expr.Literal expr)
-    {
-        return Expression.Constant(expr.Value);
-    }
+    public Expression VisitLiteralExpr(Expr.Literal expr) => Expression.Constant(expr.Value);
 
     public Expression VisitLogicalExpr(Expr.Logical expr)
     {
@@ -145,51 +163,27 @@ internal class ExpressionBuilder : Expr.IVisitor<Expression>
 
         return expr.Operator.TokenType switch
         {
-            BANG => Expression.Not(rhs),
-            MINUS => Expression.Negate(rhs),
+            BANG => Expression.Not(Expression.Convert(rhs, typeof(double))),
+            MINUS => Expression.Negate(Expression.Convert(rhs, typeof(double))),
             _ => throw new ScannerException("Shouldn't be able to get here")
         };
     }
 
     public Expression VisitVariableExpr(Expr.Variable expr)
     {
-        // can be turned into a simple Contains
-        //if (variables != null && variables.TryGetValue(expr.Name.Lexeme, out var value))
-        //{
-            if (_parameters.TryGetValue(expr.Name.Lexeme, out var p))
-            {
-                return p;
-            }
+        if (_parameters.TryGetValue(expr.Name.Lexeme, out var p))
+        {
+            return p;
+        }
 
-            //var type = value switch
-            //{
-            //    string => typeof(string),
-            //    double => typeof(double),
-            //    bool => typeof(bool),
-            //    _ => throw new ScannerException($"Unsupported variable type: '{expr.Name.Lexeme}'")
-            //};
+        var parameter = Expression.Parameter(typeof(object), expr.Name.Lexeme);
 
-            var parameter = Expression.Parameter(typeof(object), expr.Name.Lexeme);
+        _parameters.Add(expr.Name.Lexeme, parameter);
 
-            _parameters.Add(expr.Name.Lexeme, parameter);
-
-            return parameter;
-        //}
-
-        //throw new ScannerException($"Unknown variable '{expr.Name.Lexeme}'");
+        return parameter;
     }
 
     public List<ParameterExpression> GetParameters() => _parameters.Values.ToList();
-
-    //private static void CheckNumberOperands(Token token, Expression left, Expression right)
-    //{
-    //    if (left.Type == typeof(double) && right.Type == typeof(double))
-    //    {
-    //        return;
-    //    }
-
-    //    throw ScannerException.TokenMessage(token, "Operands must be numbers");
-    //}
 
     private static (Expression lhc, Expression rhc) Conversions<T>(Expression lhs, Expression rhs)
     {
@@ -202,16 +196,11 @@ internal static class Helpers
 {
     internal static object Plus(object lhs, object rhs)
     {
-        if (lhs is double d1 && rhs is double d2)
+        return lhs switch
         {
-            return d1 + d2;
-        }
-
-        if (lhs is string s1 && rhs is string s2)
-        {
-            return s1 + s2;
-        }
-
-        throw new ScannerException("Operands must be two numbers or two strings");
+            double d1 when rhs is double d2 => d1 + d2,
+            string s1 when rhs is string s2 => s1 + s2,
+            _ => throw new ScannerException("Operands must be two numbers or two strings")
+        };
     }
 }
