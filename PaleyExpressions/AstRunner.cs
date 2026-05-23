5e68baf1c9b3c57c3177a08b59f90a42d8071c88
PaleyX <jonathan.palethorpe@hotmail.co.uk>
2026-05-23 13:37:40 +0100

    added token tests

--- PaleyExpressions/AstRunner.cs
+++ PaleyExpressions/AstRunner.cs
@@ -1,35 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace PaleyExpressions;
 
-namespace PaleyExpressions
+public class AstRunner(string source, Type? functions)
 {
-    public class AstRunner(string source, Type? functions)
+    private static Interpreter? _interpreter;
+    private Expr? _expr;
+
+    public void GetAst()
     {
-        private readonly string _source = source;
-        private Interpreter _interpreter;
-        private Expr? _expr;
+        _expr = Parse(source, functions);
+    }
 
-        public void GetAst()
-        {
-            _expr = Parse(_source, functions);
-        }
+    public object? Interpret(Dictionary<string, object?>? variables)
+    {
+        _interpreter ??= new Interpreter();
 
-        public object? Interpret(Dictionary<string, object?>? variables)
-        {
-            _interpreter ??= new Interpreter();
+        return _interpreter.Interpret(_expr, variables);
+    }
 
-            return _interpreter.Interpret(_expr, variables);
-        }
+    private static Expr Parse(string source, Type? functions = null)
+    {
+        var tokens = new Scanner(source).ScanTokens();
+        var parser = new Parser(tokens, functions);
 
-        private static Expr Parse(string source, Type? functions = null)
-        {
-            var tokens = new Scanner(source).ScanTokens();
-            var parser = new Parser(tokens, functions);
-
-            return parser.Parse();
-        }
+        return parser.Parse();
     }
 }
