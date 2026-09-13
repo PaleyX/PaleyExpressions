bb55925d36af791bdb03d8b649af8c0f8e43c9f0
PaleyX <jonathan.palethorpe@hotmail.co.uk>
2026-09-13 11:19:10 +0100

    Added AST test for simple grouping

--- UnitTests/ParserTests.cs
+++ UnitTests/ParserTests.cs
@@ -1,9 +1,45 @@
 ﻿using PaleyExpressions;
+using System.Linq.Expressions;
 
 namespace UnitTests;
 
 public class ParserTests
 {
+    [Fact]
+    public void AstIsCreatedCorrectlyForGrouping()
+    {
+        var tokens = new Scanner("10*(6+5)").ScanTokens();
+        var parser = new Parser(tokens);
+
+        var expression = parser.Parse();
+
+        // Top level
+        var binary = Assert.IsType<Expr.Binary>(expression);
+        Assert.Equal(TokenType.STAR, binary.Operator.TokenType);
+
+        // LHS
+        var literal = Assert.IsType<Expr.Literal>(binary.Left);
+        var value = Assert.IsType<double>(literal.Value);
+        Assert.Equal(10d, value);
+
+        // RHS
+        var grouping = Assert.IsType<Expr.Grouping>(binary.Right);
+        binary = Assert.IsType<Expr.Binary>(grouping.Expression);
+
+        // Grouping top level
+        Assert.Equal(TokenType.PLUS, binary.Operator.TokenType);
+
+        // Grouping LHS
+        literal = Assert.IsType<Expr.Literal>(binary.Left);
+        value = Assert.IsType<double>(literal.Value);
+        Assert.Equal(6d, value);
+
+        // Grouping RHS
+        literal = Assert.IsType<Expr.Literal>(binary.Right);
+        value = Assert.IsType<double>(literal.Value);
+        Assert.Equal(5d, value);
+    }
+
     [Theory]
     [InlineData("1!", "!")]
     public void UnexpectedTokenThrowsCorrectly(string expression, string unexpected)
@@ -11,7 +47,7 @@ public class ParserTests
         var tokens = new Scanner(expression).ScanTokens();
         var parser = new Parser(tokens);
 
-        var ex = Assert.Throws<ExpressionException>(() => parser.Parse());
+        var ex = Assert.Throws<ExpressionException>(parser.Parse);
 
         Assert.Equal($"Unexpected token: at '{unexpected}'", ex.Message);
     }
@@ -23,7 +59,7 @@ public class ParserTests
         var tokens = new Scanner(expression).ScanTokens();
         var parser = new Parser(tokens);
 
-        var ex = Assert.Throws<ExpressionException>(() => parser.Parse());
+        var ex = Assert.Throws<ExpressionException>(parser.Parse);
 
         Assert.Equal($"Expect expression: at '{unexpected}'", ex.Message);
     }
@@ -39,7 +75,7 @@ public class ParserTests
         var tokens = new Scanner(expression).ScanTokens();
         var parser = new Parser(tokens);
 
-        var ex = Assert.Throws<ExpressionException>(() => parser.Parse());
+        var ex = Assert.Throws<ExpressionException>(parser.Parse);
 
         Assert.Equal($"{unexpected} at end", ex.Message);
     }
