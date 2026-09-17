dee49c87cda3adec2fc4724a9fe78cbf65822669
PaleyX <jonathan.palethorpe@hotmail.co.uk>
2026-09-17 13:43:56 +0100

    some mopping up before release

--- UnitTests/UserFunctionsTests.cs
+++ UnitTests/UserFunctionsTests.cs
@@ -17,12 +17,21 @@ public class UserFunctionsTests
         Assert.Equal(1d, astResult);
         Assert.Equal(1d, expResult);
 
-        // Use user defined version of abs
+        // Use user defined version of abs replaces version in Builtins
         astResult = new AstRunner(expression, typeof(UserFunctions)).Interpret();
         expResult = new ExpressionRunner(expression, typeof(UserFunctions)).Interpret();
 
         Assert.Equal(1_000_000d, astResult);
         Assert.Equal(1_000_000d, expResult);
+
+        // upper function in Builtins is still accessible 
+        const string upper = "upper(\"hello\")";
+
+        astResult = new AstRunner(upper, typeof(UserFunctions)).Interpret();
+        expResult = new ExpressionRunner(upper, typeof(UserFunctions)).Interpret();
+
+        Assert.Equal("HELLO", astResult);
+        Assert.Equal("HELLO", expResult);
     }
 }
 
